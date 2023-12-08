b31b3977f8f367b94187d5ed6198ac9ed506d936
Henrique <you@example.com>
2023-12-08 12:49:30 -0300

    test: implementation tests for escala

---
+++ tests/test_escalas.py
@@ -0,0 +1,71 @@
+"""
+AAA - 3A
+
+Arange - Act - Asserts
+Arrumar - Agir - Garantir
+"""
+
+from pytest import mark, raises
+
+from notas_musicais.escalas import ESCALAS, NOTAS, escala
+
+def test_deve_funcionar_com_notas_minusculas():
+    # Arrumar
+    tonica = 'c'
+    tonalidade = 'maior'
+
+    # Act - Chamo o que testar
+    result = escala(tonica, tonalidade)
+
+    # Assert
+    assert result
+
+def test_deve_retornar_um_erro_informando_que_a_nota_nao_existe():
+    tonica = 'X'
+    tonalidade = 'maior'
+
+    mensagem_de_erro = f'Essa nota não existe, tente uma dessas {NOTAS}'
+
+    with raises(ValueError) as error:
+        escala(tonica, tonalidade)
+    
+    assert mensagem_de_erro == error.value.args[0]
+
+def test_deve_retornar_um_erro_informando_que_a_escala_nao_existe():
+    tonica = 'c'
+    tonalidade = 'tonalidade'
+
+    mensagem_de_erro = f'Essa escala não existe ou não foi implementada, tente uma dessas {list(ESCALAS.keys())}'
+
+    with raises(KeyError) as error:
+        escala(tonica, tonalidade)
+    
+    assert mensagem_de_erro == error.value.args[0]
+
+@mark.parametrize(
+    'tonica,tonalidade, esperado',
+    [
+        ('C', 'maior', ['C', 'D', 'E', 'F', 'G', 'A', 'B']),
+        ('C#', 'maior', ['C#', 'D#', 'F', 'F#', 'G#', 'A#', 'C']),
+        ('D', 'maior', ['D', 'E', 'F#', 'G', 'A', 'B', 'C#']),
+        ('D#', 'maior', ['D#', 'F', 'G', 'G#', 'A#', 'C', 'D']),
+        ('E', 'maior', ['E', 'F#', 'G#', 'A', 'B', 'C#', 'D#']),
+        ('F', 'maior', ['F', 'G', 'A', 'A#', 'C', 'D', 'E']),
+        ('F#', 'maior', ['F#', 'G#', 'A#', 'B', 'C#', 'D#', 'F']),
+        ('G', 'maior', ['G', 'A', 'B', 'C', 'D', 'E', 'F#']),
+        ('G#', 'maior', ['G#', 'A#', 'C', 'C#', 'D#', 'F', 'G']),
+        ('A', 'maior', ['A', 'B', 'C#', 'D', 'E', 'F#', 'G#']),
+        ('A#', 'maior', ['A#', 'C', 'D', 'D#', 'F', 'G', 'A']),
+        ('B', 'maior', ['B', 'C#', 'D#', 'E', 'F#', 'G#', 'A#']),
+    ],
+)
+
+def test_deve_retornar_as_notas_corretas(tonica, tonalidade, esperado):
+    resultado = escala(tonica, tonalidade)
+    assert resultado['notas'] == esperado
+
+def test_deve_retornar_os_sete_graus():
+    esperado = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']
+    resultado = escala('c', 'maior')
+    
+    assert resultado['graus'] == esperado
